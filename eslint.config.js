760ed6fa980d90ba402961df6d9ea352884874f8
e22m4u <e22m4u@yandex.ru>
2026-01-29 14:15:39 +0300

    fix: lint

--- eslint.config.js
+++ eslint.config.js
@@ -28,7 +28,7 @@ export default [{
     ...eslintMochaPlugin.configs.recommended.rules,
     ...eslintChaiExpectPlugin.configs['recommended-flat'].rules,
     ...eslintJsdocPlugin.configs['flat/recommended-error'].rules,
-    "curly": "error",
+    'curly': 'error',
     'no-duplicate-imports': 'error',
     'import/export': 0,
     'jsdoc/reject-any-type': 0,
